795fea499844625c9a95c4f705e1e07114c4ef74
robertolagreca <robertolagreca92@gmail.com>
2022-11-08 15:07:14 +0100

    Parte grafica + password bonus

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let nome, cognome, colore_preferito;
+let nome, cognome, colore_preferito, numero1, numero2;
+let divisione, st_password, bon_password;
 
 nome = prompt("Inserisci il nome");
 cognome = prompt("Inserisci il cognome");
 colore_preferito = prompt("Inserisci il tuo colore preferito");
+numero1 = prompt("Inserisci un numero");
+numero2 = prompt("Inserisci un altro numero");
+divisione= numero1 / numero2;
+
+
+st_password = nome + cognome + colore_preferito + "21";
+bon_password = nome + cognome + colore_preferito + divisione;
 
 console.log (
     `
@@ -15,6 +23,19 @@ console.log (
     Colore preferito: ${colore_preferito}
 
     ------------ Password Gen ------------
-    Password: ${nome + cognome + colore_preferito + "21"}
+    --------------------------------------
+    --------------------------------------
+    --------------------------------------
+    Password standard: ${nome + cognome + colore_preferito + "21"}
+    --------------------------------------
+    --------------------------------------
+    --------------------------------------
+    Password bonus: ${nome + cognome + colore_preferito + numero1/numero2}
+    --------------------------------------
+    -------------------------------------- 
+    --------------------------------------
     `
 );
+
+document.getElementById("st_password").innerHTML = st_password;
+document.getElementById("bon_password").innerHTML = bon_password;
